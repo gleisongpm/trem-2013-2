18b8c66c8b1368e98139d00b323a2a5a21c87445
gleisongpm <gleisongpm@hotmail.com>
2014-02-09 23:40:29 -0200

    Criação do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trem::Application.config.secret_key_base = 'f5d8a2250ff895f6128150f2efeacf094e4823586e848c9e5268ec609180694cf1169ace684c7ed0893e21cc92d497de8de04c93c62a2b81987f0551e9a35eb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trem::Application.config.session_store :cookie_store, key: '_trem_session'
